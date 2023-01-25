81baad5f634c1d4089a7af638c9c2139fe7ac393
AbdalrahmanA <ali.abdalrahman@mhr.co.uk>
2023-01-25 12:20:06 +0000

    removed white space and commented out code

--- src/app/table/business-components/table/table.component.ts
+++ src/app/table/business-components/table/table.component.ts
@@ -14,8 +14,6 @@ import {ITableConfig} from "./interfaces/table-config.interface";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent implements OnInit {
-  // @Input()
-  // public mhrTableConfig: ITableConfig;
 
    headers: Array<string> = ['id','bookName', 'releaseDate', 'description', 'pageCount', 'author', 'Rating'];
    dataToDisplay: Array<IBook> = [];
@@ -82,90 +80,4 @@ export class TableComponent implements OnInit {
       this.dataToDisplay[bookIndex] = updatedBook;
       this._changeDetectorRef.markForCheck();
     });
-  }
-
-
-
-
-
-
-
-
-}
-
-  // search(searchTerm: string) {
-  //   this.editHero = undefined;
-  //   if (searchTerm) {
-  //     this.heroesService
-  //       .searchHeroes(searchTerm)
-  //       .subscribe((heroes) => (this.heroes = heroes));
-  //   } else {
-  //     this.getHeroes();
-  //   }
-  // }
-// }
-
-// const ELEMENT_DATA: IBook[] = [
-//   {
-//     'id': "1",
-//     'bookName': 'Harry Potter',
-//     'releaseDate': 'March 19, 2000',
-//     'description': 'lorem suesh aued huisk deikm.',
-//     'pageCount': 300,
-//     'author': "300",
-//     'Rating': 300,
-//   }, {
-//     'id': "2",
-//     'bookName': 'To Kill a Mockingbird',
-//     'releaseDate': 'March 18, 2021',
-//     'description': '15 gallon capacity rolling garden cart',
-//     'pageCount': 4.2,
-//     'author': "4.2",
-//     'Rating': 4.2,
-//   }
-// ];
-// const ELEMENT_DATA: IBook[] = [
-//
-//   {
-//     'id': "1",
-//     'bookName': 'Harry Potter',
-//     'releaseDate': 'March 19, 2000',
-//     'description': 'lorem suesh aued huisk deikm.',
-//     'price': 19.95,
-//     'starRating': 3.2,
-//   }, {
-//     'id': "2",
-//     'bookName': 'To Kill a Mockingbird',
-//     'releaseDate': 'March 18, 2021',
-//     'description': '15 gallon capacity rolling garden cart',
-//     'price': 32.99,
-//     'starRating': 4.2,
-//   }
-// ];
-
-
-
-// constructor(private bookService: BookService) {
-// }
-// public title = 'LibApp';
-// public imageWidth: number =50;
-// public imageMargin: number= 2;
-// public showImage: boolean=false;
-//
-// private _listFilter: string = '';
-//
-// get listFilter(): string{
-//   return this._listFilter;
-// }cd .
-//
-// set listFilter(value: string){
-//   this._listFilter = value;
-//   console.log(value)
-//   this.filteredBooks = this.performFilter(value)
-// }
-//
-//
-// public filteredBooks: IBook[] =[];
-// public books: IBook[] =[
-// ];
-//
+  }}
